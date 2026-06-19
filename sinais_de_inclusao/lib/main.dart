054b6e9335ba436a4c391426f2985b4a4fe7d90f
VicBenfica <vick5.5@hotmail.com>
2026-06-19 16:13:27 -0300

    cadastro categoria

--- sinais_de_inclusao/lib/main.dart
+++ sinais_de_inclusao/lib/main.dart
@@ -1,6 +1,7 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
+import 'package:sinais_de_inclusao/modules/categoria/cadastro_categoria.dart';
 import 'package:sinais_de_inclusao/modules/home/home_page.dart';
 import 'package:sinais_de_inclusao/modules/login/login_page.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
           seedColor: const Color(0xFFFFB76F),
         ),
       ),
-      home: const LoginPage(), 
+      home: const CadastroCategoriaPage(), 
     );
   }
 }
