1f309ab5cbaef2ec78b1359180f0daf78e3fa0d8
VicBenfica <vick5.5@hotmail.com>
2026-06-20 22:21:46 -0300

    Edicao de Questao

--- sinais_de_inclusao/lib/main.dart
+++ sinais_de_inclusao/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:sinais_de_inclusao/modules/categoria/cadastro_categoria.dart';
 import 'package:sinais_de_inclusao/modules/cadastro/cadastro_page.dart';
 import 'package:sinais_de_inclusao/modules/home/home_page.dart';
 import 'package:sinais_de_inclusao/modules/login/login_page.dart';
+import 'package:sinais_de_inclusao/modules/questoes/edicao_questao.dart';
 import 'package:sinais_de_inclusao/modules/temas/tema_page.dart';
 
 void main() {
@@ -25,15 +26,17 @@ class MyApp extends StatelessWidget {
           seedColor: const Color(0xFFFFB76F),
         ),
       ),
-      initialRoute: '/temas',
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/cadastro': (context) => const CadastroPage(),
-        '/home': (context) => const HomePage(),
-        '/cadastro-categoria': (context) => const CadastroCategoriaPage(),
-        '/edicao-categoria': (context) => const EdicaoCategoriaPage(),
-        '/temas': (context) => const TemasPage(),
-      },
+      home: EdicaoQuestaoPage(),
+      // initialRoute: '/temas',
+      // routes: {
+      //   '/login': (context) => const LoginPage(),
+      //   '/cadastro': (context) => const CadastroPage(),
+      //   '/home': (context) => const HomePage(),
+      //   '/cadastro-categoria': (context) => const CadastroCategoriaPage(),
+      //   '/edicao-categoria': (context) => const EdicaoCategoriaPage(),
+      //   '/edicao-questao': (context) => const EdicaoQuestaoPage(),
+      //   '/temas': (context) => const TemasPage(),
+      // },
     );
   }
 }
