a5d45a181dd2a0e601ca5b1453a7d573dde26cc0
VicBenfica <vick5.5@hotmail.com>
2026-09-10 16:36:26 -0300

    favoritar

--- sinais_de_inclusao/lib/main.dart
+++ sinais_de_inclusao/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:sinais_de_inclusao/modules/categoria/edicao_categoria.dart';
 import 'package:sinais_de_inclusao/modules/categoria/cadastro_categoria.dart';
 import 'package:sinais_de_inclusao/modules/cadastro/cadastro_page.dart';
 import 'package:sinais_de_inclusao/modules/categoria/excluir_categoria.dart';
+import 'package:sinais_de_inclusao/modules/favoritos/favoritos_page.dart';
 import 'package:sinais_de_inclusao/modules/home/home_page.dart';
 import 'package:sinais_de_inclusao/modules/listarQuestoes/listagem_questoes_page.dart';
 import 'package:sinais_de_inclusao/modules/login/login_page.dart';
@@ -43,7 +44,8 @@ class MyApp extends StatelessWidget {
         '/temas': (context) => const TemasPage(),
         '/listagem_questoes': (context) => const ListagemQuestoesPage(),
         '/cadastro-questao': (context) => const CadastroQuestaoPage(),
-        '/trilha': (context) => const TrilhaPage()
+        '/trilha': (context) => const TrilhaPage(),
+        '/favoritos': (context) => const FavoritosPage()
       }
     );
   }
--- sinais_de_inclusao/lib/widgets/custom_app_bar.dart
+++ sinais_de_inclusao/lib/widgets/custom_app_bar.dart
@@ -37,25 +37,36 @@ class _CustomAppBarState extends State<CustomAppBar> {
       toolbarHeight: 100,
       backgroundColor: const Color(0xFF623FBD),
       elevation: 0,
+
       title: Image.asset('assets/images/logocirculo.png', height: 70),
+
       actions: [
         PopupMenuButton<String>(
           icon: const Icon(Icons.menu, color: Colors.white, size: 35),
+
           onSelected: (value) async {
             if (value == 'temas') {
               Navigator.pushNamed(context, '/temas');
+            } else if (value == 'favoritos') {
+              Navigator.pushNamed(context, '/favoritos');
             } else if (value == 'logout') {
               final prefs = await SharedPreferences.getInstance();
+
               await prefs.remove('token');
               await prefs.remove('role');
 
               if (context.mounted) {
-                Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  '/home',
+                  (route) => false,
+                );
               }
             }
           },
+
           itemBuilder: (BuildContext context) => [
-            if (_autenticado)
+            if (_autenticado) ...[
               const PopupMenuItem<String>(
                 value: 'temas',
                 child: Row(
@@ -66,20 +77,36 @@ class _CustomAppBarState extends State<CustomAppBar> {
                   ],
                 ),
               ),
-            const PopupMenuItem<String>(
-              value: 'logout',
-              child: Row(
-                children: [
-                  Icon(Icons.logout, color: Colors.red),
-                  SizedBox(width: 10),
-                  Text("Sair"),
-                ],
+
+              const PopupMenuItem<String>(
+                value: 'favoritos',
+                child: Row(
+                  children: [
+                    Icon(Icons.favorite, color: Color(0xFF623FBD)),
+                    SizedBox(width: 10),
+                    Text("Favoritos"),
+                  ],
+                ),
               ),
-            ),
+
+              // SAIR
+              const PopupMenuItem<String>(
+                value: 'logout',
+                child: Row(
+                  children: [
+                    Icon(Icons.logout, color: Colors.red),
+                    SizedBox(width: 10),
+                    Text("Sair"),
+                  ],
+                ),
+              ),
+            ],
           ],
         ),
+
         const SizedBox(width: 10),
       ],
+
       automaticallyImplyLeading: false,
     );
   }
