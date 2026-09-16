5d5f373363b4e7b51bf4d703e7d40ffe7bd366d2
serrabruna <brunaibserra@hotmail.com>
2026-09-16 16:16:47 -0300

    limpeza de imports duplicados

--- sinais_de_inclusao/lib/modules/pratica/pratica_page.dart
+++ sinais_de_inclusao/lib/modules/pratica/pratica_page.dart
@@ -1,9 +1,6 @@
 import 'dart:math';
 import 'dart:async';
-import 'dart:math';
 
-import 'package:flutter/material.dart';
-import 'package:ultralytics_yolo/ultralytics_yolo.dart';
 import 'package:flutter/material.dart';
 import 'package:ultralytics_yolo/ultralytics_yolo.dart';
 
